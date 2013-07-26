f9743b2ae895741f0f51ec32279bf55a7b206f7a
Brad Vincent <bradvin@gmail.com>
2013-07-26 17:08:38 +0200

    first commit (plugin header)

---
+++ foodocs.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: FooDocs
+Plugin URI: https://github.com/fooplugins/foodocs
+Description: Organized documentation for your digital products
+Author: FooPlugins
+Author URI: http://fooplugins.com
+Version: 0.1-alpha
+*/
